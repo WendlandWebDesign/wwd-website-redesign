4bc89e3721f164c0e14b557cbf7da8de9efc0dba
Artis Warratz <artiswarratz2005@gmail.com>
2026-02-08 17:17:21 +0100

    commit

--- index.php
+++ index.php
@@ -90,7 +90,50 @@ get_header();
 
 
 
-
+<div class="one-img-layout-holder">
+    <h3 class="mw-small">Weniger Aufwand. Mehr Zeit.</h3>
+    <div class="one-img-layout mw-small">
+        <div class="txt-holder">
+            <p class="light">KI hilft dabei, wiederkehrende Aufgaben automatisch zu erledigen – zuverlässig und im Hintergrund. Sie unterstützt Ihr Unternehmen dort, wo täglich Zeit verloren geht: E-Mails werden sortiert, Anfragen strukturiert und Informationen direkt weiterverarbeitet. So bleibt mehr Zeit für das, was wirklich wichtig ist.
+            </p>
+        </div>
+        <div class="img-holder">
+            <img src="<?php echo esc_url(get_option('weg-zur-website-1')); ?>" alt="">
+        </div>
+        <div class="bottom-holder">
+            <ul>
+                <li>
+                    <img src="<?php echo esc_url( get_template_directory_uri() . '/assets/icons/check.svg' ); ?>" alt="">
+                    <p class="light">Weniger manuelle Arbeit im Alltag</p>
+                </li>
+                <li>
+                    <img src="<?php echo esc_url( get_template_directory_uri() . '/assets/icons/check.svg' ); ?>" alt="">
+                    <p class="light">Ordnung statt Chaos im Posteingang</p>
+                </li>
+                <li>
+                    <img src="<?php echo esc_url( get_template_directory_uri() . '/assets/icons/check.svg' ); ?>" alt="">
+                    <p class="light">Automatisierte Abläufe rund um die Uhr</p>
+                </li>
+                <li>
+                    <img src="<?php echo esc_url( get_template_directory_uri() . '/assets/icons/check.svg' ); ?>" alt="">
+                    <p class="light">Effizientere Prozesse ohne zusätzliches Personal</p>
+                </li>
+                <button onclick="window.location.href='<?php echo esc_url( home_url( '/kontakt/' ) ); ?>'" class="btn light">
+                    <span class="btn__border" aria-hidden="true">
+                        <svg class="btn__svg" viewBox="0 0 100 40" preserveAspectRatio="none">
+                            <path class="btn__path" d="M2,2 H98 Q100,2 100,4 V36 Q100,38 98,38 H2 Q0,38 0,36 V4 Q0,2 2,2 Z"/>
+                            <path class="btn__seg btn__seg--1" vector-effect="non-scaling-stroke" d="M2,2 H98 Q100,2 100,4 V36 Q100,38 98,38 H2 Q0,38 0,36 V4 Q0,2 2,2 Z"/>
+                            <path class="btn__seg btn__seg--2" vector-effect="non-scaling-stroke" d="M2,2 H98 Q100,2 100,4 V36 Q100,38 98,38 H2 Q0,38 0,36 V4 Q0,2 2,2 Z"/>
+                            <path class="btn__seg btn__seg--3" vector-effect="non-scaling-stroke" d="M2,2 H98 Q100,2 100,4 V36 Q100,38 98,38 H2 Q0,38 0,36 V4 Q0,2 2,2 Z"/>
+                            <path class="btn__seg btn__seg--4" vector-effect="non-scaling-stroke" d="M2,2 H98 Q100,2 100,4 V36 Q100,38 98,38 H2 Q0,38 0,36 V4 Q0,2 2,2 Z"/>
+                        </svg>
+                    </span>
+                    <p><?php echo wwd_inline_svg( 'arrow-white.svg', array( 'class' => 'icon--arrow-white', 'aria_hidden' => true ) ); ?>zu den KI-Lösungen</p>
+                </button>
+            </ul>
+        </div>
+    </div>
+</div>
 
 
 
