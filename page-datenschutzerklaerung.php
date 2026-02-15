d55d8809c571051081a3987ffc1a6829cb80d91f
Liam <liamwgit@gmail.com>
2026-02-15 18:54:56 +0100

    dsgvo

--- page-datenschutzerklaerung.php
+++ page-datenschutzerklaerung.php
@@ -2,7 +2,34 @@
 get_header();
 ?>
 <main>
-    
+    <?php
+    $datenschutz_query = new WP_Query(
+        array(
+            'post_type'      => 'dsgvo',
+            'post_status'    => 'publish',
+            'name'           => 'datenschutzerklaerung',
+            'posts_per_page' => 1,
+            'no_found_rows'  => true,
+        )
+    );
+
+    if ( $datenschutz_query->have_posts() ) :
+        while ( $datenschutz_query->have_posts() ) :
+            $datenschutz_query->the_post();
+            ?>
+
+            <div class="dsgvo-holder light">
+                <div class="datenschutz-content mw">
+                <?php the_content(); ?>
+                </div>
+            </div>
+
+            <?php
+        endwhile;
+    endif;
+
+    wp_reset_postdata();
+    ?>
 </main>
 <?php
 get_footer();
